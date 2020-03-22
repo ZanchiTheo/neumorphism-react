135bca47934484aa636d4f1666089a45556d99a7
Théo Zanchi <tzanchi@appstud.com>
2020-03-22 14:14:37 +0100

    [feat] setup rollup

---
+++ rollup.config.js
@@ -0,0 +1,122 @@
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
+import external from 'rollup-plugin-peer-deps-external'
+import { terser } from 'rollup-plugin-terser'
+import packageJSON from './package.json'
+
+const input = './src/index.js'
+const minifyExtension = pathToFile => pathToFile.replace(/\.js$/, ".min.js");
+
+export default [
+  // CommonJS
+  {
+    input,
+    output: {
+      file: packageJSON.main,
+      format: 'cjs',
+    },
+    plugins: [
+      babel({
+        exclude: 'node_modules/**',
+      }),
+      external(),
+      resolve(),
+      commonjs(),
+    ],
+  },
+  {
+    input,
+    output: {
+      file: minifyExtension(packageJSON.main),
+      format: "cjs"
+    },
+    plugins: [
+      babel({
+        exclude: "node_modules/**"
+      }),
+      external(),
+      resolve(),
+      commonjs(),
+      terser()
+    ]
+  },
+  // UMD
+  {
+    input,
+    output: {
+      file: packageJSON.browser,
+      format: "umd",
+      name: "reactNeumorphismLibrary",
+      globals: {
+        react: "React",
+        "@emotion/styled": "styled",
+        "@emotion/core": "core"
+      }
+    },
+    plugins: [
+      babel({
+        exclude: "node_modules/**"
+      }),
+      external(),
+      resolve(),
+      commonjs()
+    ]
+  },
+  {
+    input,
+    output: {
+      file: minifyExtension(packageJSON.browser),
+      format: "umd",
+      name: "reactNeumorphismLibrary",
+      globals: {
+        react: "React",
+        "@emotion/styled": "styled",
+        "@emotion/core": "core"
+      }
+    },
+    plugins: [
+      babel({
+        exclude: "node_modules/**"
+      }),
+      external(),
+      resolve(),
+      commonjs(),
+      terser()
+    ]
+  },
+  // ES
+  {
+    input,
+    output: {
+      file: packageJSON.module,
+      format: "es",
+      exports: "named"
+    },
+    plugins: [
+      babel({
+        exclude: "node_modules/**"
+      }),
+      external(),
+      resolve(),
+      commonjs()
+    ]
+  },
+  {
+    input,
+    output: {
+      file: minifyExtension(packageJSON.module),
+      format: "es",
+      exports: "named"
+    },
+    plugins: [
+      babel({
+        exclude: "node_modules/**"
+      }),
+      external(),
+      resolve(),
+      commonjs(),
+      terser()
+    ]
+  },
+]
---
+++ src/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/prefer-default-export */
+export { default as NeuDiv } from './components/NeuDiv/NeuDiv'
+export { default as NeuButton } from './components/NeuButton/NeuButton'
+export { default as NeuClock } from './components/NeuClock/NeuClock'
+export { default as NeuSlider } from './components/NeuSlider/NeuSlider'
+export { default as NeuProgress } from './components/NeuProgress/NeuProgress'
+export { default as NeuRadio } from './components/NeuRadio/NeuRadio'
+export { default as NeuToggle } from './components/NeuToggle/NeuToggle'
+export { default as NeuTextInput } from './components/NeuTextInput/NeuTextInput'
