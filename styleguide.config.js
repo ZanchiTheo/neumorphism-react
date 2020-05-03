331805e3928d25e282b72d94799d4b4f2844b221
Théo Zanchi <tzanchi@appstud.com>
2020-05-03 19:20:31 +0200

    changed styleguidist setup

--- styleguide.config.js
+++ styleguide.config.js
@@ -7,7 +7,10 @@ module.exports = {
         {
           test: /\.jsx?$/,
           exclude: /node_modules/,
-          loader: 'babel-loader'
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
         },
         {
           test: /\.svg$/,
